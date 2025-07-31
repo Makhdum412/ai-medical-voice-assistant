9d1fdd017708b1445420e8eb85ac9081a293d415
Makhdum412 <hossainmakhdum2004@gmail.com>
2025-07-31 21:33:29 +0530

    Update HistoryList.tsx and add medical-assistance image

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { dimensionValueTypes } from 'framer-motion';
+import React, { useState } from 'react'
+import Image from 'next/image'
 
 function HistoryList() {
     const [historyList, setHistoryList] = useState([]);
   return (
-    <div>HistoryList</div>
+    <div>
+      {historyList.length==0?
+      <div>
+        <Image src={'/medical-assistance.png'} alt="empty" width={100} height={200} />
+      </div>
+      :<div>
+        List
+       </div>
+}
+</div>
   )
 }
 
